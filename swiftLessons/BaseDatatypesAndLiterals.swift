429c4cd575259274c013cf022d5f4e87411cbd79
alehbarushka <1.alehbarushka@gmail.com>
2024-12-04 11:28:37 +0300

    2

--- swiftLessons/BaseDatatypesAndLiterals.swift
+++
@@ -1,12 +0,0 @@
-import Foundation
-
-func dataTypesTest() {
-    1 // Int
-    1.0 // Double
-    "привет" // String
-    true // Boolean
-    false // Boolean
-    
-    Character("c") // Char
-    Float(1.2) // Float
-}
--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,3 +1,3 @@
 import Foundation
 
-dataTypesTest()
+print("привет", "Мир", "!")
