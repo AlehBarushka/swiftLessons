06c84a199389a4a99c52c6536134adfb99625138
alehbarushka <1.alehbarushka@gmail.com>
2024-12-05 10:31:42 +0300

    3

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,3 +1,21 @@
 import Foundation
 
-print("привет", "Мир", "!")
+let a: Int = 1
+let b: Double = 3.4
+let c: Float = 2.5 // явно указать
+let d: String = "string"
+let e: Character = "s" // явно указать
+let f: Bool = true
+
+var a1: Int = 1
+a1 = 2
+var b1: Double = 3.4
+b1 = 3.5
+var c1: Float = 2.5
+c1 = 2.6
+var d1: String = "string"
+d1 = "string1"
+var e1: Character = "s"
+e1 = "b"
+var f1: Bool = true
+f1 = false
