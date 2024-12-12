3422a0f122390f9837936706798f65c7b62bc835
alehbarushka <1.alehbarushka@gmail.com>
2024-12-12 22:25:17 +0300

    5

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,14 +1,27 @@
 import Foundation
 
-let age = 26
-let name = "Иван"
+let age = 32
+let name = "Антон"
 
-let result1 = age == 26 && name == "Ivan" // false
-let result2 = age == 26 || name == "Ivan" // true
+if age < 30 {
+    print("Ты молод!")
+} else if age >= 16 && age <= 18 {
+    print("Ты слишком молод")
+} else {
+    print("Уже старый")
+}
 
-print(result1)
-print(result2)
+switch age {
+case ..<18: print("Молод")
+case 18..<25: print("Норм")
+case 25...: print("Старый")
+default: print("Блок кода не выполниться")
+}
 
-// ------
+switch name {
+case "Олег": print("Нашёл")
+default: print("Не нашёл")
+}
 
-print("1" + "2") // 12
+let message = age < 20 ? "слишком молод" : "слишком стар"
+print(message)
