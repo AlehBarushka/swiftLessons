cd5d21a25b63b976cd6452de7a26ef948eac4d30
alehbarushka <1.alehbarushka@gmail.com>
2024-12-13 11:58:17 +0300

    6

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,27 +1,4 @@
-import Foundation
+print("Веедите своё имя: ")
+let string = readLine() ?? ""
 
-let age = 32
-let name = "Антон"
-
-if age < 30 {
-    print("Ты молод!")
-} else if age >= 16 && age <= 18 {
-    print("Ты слишком молод")
-} else {
-    print("Уже старый")
-}
-
-switch age {
-case ..<18: print("Молод")
-case 18..<25: print("Норм")
-case 25...: print("Старый")
-default: print("Блок кода не выполниться")
-}
-
-switch name {
-case "Олег": print("Нашёл")
-default: print("Не нашёл")
-}
-
-let message = age < 20 ? "слишком молод" : "слишком стар"
-print(message)
+print(string)
