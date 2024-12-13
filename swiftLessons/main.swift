915902d548528c36ede6d0bf49b400bc7993772e
alehbarushka <1.alehbarushka@gmail.com>
2024-12-13 13:07:57 +0300

    7

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,4 +1,38 @@
-print("Веедите своё имя: ")
-let string = readLine() ?? ""
+var a: Int = 1
+var b: Int? = 1
+var c: Int? = nil
 
-print(string)
+// let c = a + b Error
+
+// Explicity unwrap
+if let b {  // let b = b
+    print(a + b)
+} else {
+    print("В b нет значения")
+}
+
+if let c {  // let c = c
+    print(a + c)
+} else {
+    print("В c нет значения")
+}
+
+if let c, let b {
+    print(c + b)
+} else {
+    print("нет значения")
+}
+
+// Force unwrap
+print(a + b!)
+//print(a + c!) Runtime Error
+
+
+// Default value
+if let c {
+    print(c + a)
+} else {
+    print(0 + a)
+}
+
+print((c ?? 0) + a)
