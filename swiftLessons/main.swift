ace04910f82581b5cee107d968d36c5af29135ce
alehbarushka <1.alehbarushka@gmail.com>
2025-01-06 17:37:24 +0300

    lesson 27

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,48 +1,100 @@
 import Darwin
 import Foundation
 
-let heightArray = [202, 210, 198, 187, 165]
+let allPersons = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
+let personsInOfficeYesterday = ["Ivan", "Oleg", "Anton"]
+let personsInOfficeToday = ["Ivan", "Alex", "Anton", "Max"]
 
-let heightTuple = (Ivan: 202, Alex: 210, Oleg: 198, Anton: 187, Max: 165)
+for person in personsInOfficeToday {
+    if person == "Ivan" {
+        print("Ivan в офисе")
 
-let heighArrayTuples: [(name: String, heigth: Int)] = [
-    (name: "Ivan", heigth: 202),
-    (name: "Alex", heigth: 210),
-    (name: "Oleg", heigth: 198),
-    (name: "Anton", heigth: 187),
-    (name: "Max", heigth: 165),
-]
-
-let heighDic: [String: Int] = [
-    "Ivan": 202,
-    "Alex": 210,
-    "Oleg": 198,
-    "Anton": 187,
-    "Max": 165,
-]
-
-print(heighDic["Ivan"])
-
-// Пустой словарь
-let dicA: [String: Int] = [:]
-let dicB = [String: Int]()
-let dicC: [String: Int]?
-
-// Оперции
-var dicVar = ["a": 1, "b": 2]
-dicVar = ["b": 1, "a": 2]
-dicVar["c"] = 3
-dicVar["a"] = nil
-dicVar["b"] = 5
-
-// Удаление всех
-// dicVar.removeAll()
-
-for item in dicVar {
-    print(item.key, item.value)
+        break
+    }
 }
 
-//С деструктуризацией
-for (a, b) in dicVar {
-    print(a, b)
+// Проблема массива в том, что могут быть повторения и приходиться перебирать весь массив для поиска
+
+//Пример со словарём
+let allPersonsDic = [
+    "Ivan": "Ivan",
+    "Alex": "Alex",
+    "Oleg": "Oleg",
+    "Anton": "Anton",
+    "Max": "Max",
+]
+
+let personsInOfficeYesterdayDic = [
+    "Ivan": "Ivan",
+    "Oleg": "Oleg",
+    "Anton": "Anton",
+]
+
+let personsInOfficeTodayDic = [
+    "Ivan": "Ivan",
+    "Alex": "Alex",
+    "Anton": "Anton",
+    "Max": "Max",
+]
+
+if personsInOfficeTodayDic["Ivan"] != nil {
+    print("Ivan в офисе")
 }
+
+//Пример с мноржеством Set
+let allPersonsSet: Set = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
+var personsInOfficeYesterdaySet: Set = ["Ivan", "Oleg", "Anton"]
+var personsInOfficeTodaySet: Set = ["Ivan", "Alex", "Anton", "Max"]
+
+let result = personsInOfficeTodaySet.contains("Ivan")
+print(result)
+
+let (inserted, memberAfter) = personsInOfficeTodaySet.insert("Olga")
+print(inserted, memberAfter)
+personsInOfficeTodaySet.insert("Olga")
+personsInOfficeTodaySet.insert("Ivan")
+
+let result2 = personsInOfficeTodaySet.contains("Ivan")
+print(result2)
+print(personsInOfficeTodaySet)
+
+// Пересечение в обоих Set
+let intersection = allPersonsSet.intersection(personsInOfficeTodaySet)
+// Set вне пересечения
+let subtracting = personsInOfficeTodaySet.subtracting(
+    personsInOfficeYesterdaySet)
+// Set уникальная разница
+let symmetricDifference = personsInOfficeTodaySet.symmetricDifference(
+    personsInOfficeYesterdaySet)
+// Объединение
+let union = personsInOfficeTodaySet.union(personsInOfficeYesterdaySet)
+
+print(intersection)
+print(subtracting)
+print(union)
+print(symmetricDifference)
+
+// Синтаксис
+let a = Set<String>(["Ivan", "Alex", "Oleg", "Anton", "Max"])
+let a1 = Set(["Ivan", "Alex", "Oleg", "Anton", "Max"])
+let a2: Set<String> = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
+let b = Set(["Ivan", "Ivan", "Ivan", "Ivan", "Max"])  // Удалит при создании дубли
+print(b)
+
+// Изменение
+var c = Set(["Ivan", "Alex", "Oleg", "Anton", "Max"])
+c = ["Ivan", "Alex", "Oleg"]
+print(c)
+c.insert("Olga")
+print(c)
+c.remove("Ivan")
+print(c)
+
+// Проверка наличия
+print(c.contains("Olga"))
+
+// Перебор
+for element in c {
+    print(element)
+}
+
