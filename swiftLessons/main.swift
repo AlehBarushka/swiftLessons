18cbd21c444512d9dfe95f46fa552f2391398135
alehbarushka <1.alehbarushka@gmail.com>
2025-01-07 14:31:16 +0300

    lesson 30

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,100 +1,25 @@
 import Darwin
 import Foundation
 
-let allPersons = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
-let personsInOfficeYesterday = ["Ivan", "Oleg", "Anton"]
-let personsInOfficeToday = ["Ivan", "Alex", "Anton", "Max"]
+let a = 45
+let b = 34.56454564
 
-for person in personsInOfficeToday {
-    if person == "Ivan" {
-        print("Ivan в офисе")
+let string = "a: " + String(a) + " b: " + String(b)
 
-        break
-    }
-}
+print(string)
 
-// Проблема массива в том, что могут быть повторения и приходиться перебирать весь массив для поиска
+let concatString = "a: \(a) b: \(b) summ: \(a + 78)"
 
-//Пример со словарём
-let allPersonsDic = [
-    "Ivan": "Ivan",
-    "Alex": "Alex",
-    "Oleg": "Oleg",
-    "Anton": "Anton",
-    "Max": "Max",
-]
+print(concatString)
 
-let personsInOfficeYesterdayDic = [
-    "Ivan": "Ivan",
-    "Oleg": "Oleg",
-    "Anton": "Anton",
-]
+let doubleQuotationMarks = "Привет \"Мир\""  // Двойные кавычки
+let multilineString = """
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    """
 
-let personsInOfficeTodayDic = [
-    "Ivan": "Ivan",
-    "Alex": "Alex",
-    "Anton": "Anton",
-    "Max": "Max",
-]
-
-if personsInOfficeTodayDic["Ivan"] != nil {
-    print("Ivan в офисе")
-}
-
-//Пример с мноржеством Set
-let allPersonsSet: Set = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
-var personsInOfficeYesterdaySet: Set = ["Ivan", "Oleg", "Anton"]
-var personsInOfficeTodaySet: Set = ["Ivan", "Alex", "Anton", "Max"]
-
-let result = personsInOfficeTodaySet.contains("Ivan")
-print(result)
-
-let (inserted, memberAfter) = personsInOfficeTodaySet.insert("Olga")
-print(inserted, memberAfter)
-personsInOfficeTodaySet.insert("Olga")
-personsInOfficeTodaySet.insert("Ivan")
-
-let result2 = personsInOfficeTodaySet.contains("Ivan")
-print(result2)
-print(personsInOfficeTodaySet)
-
-// Пересечение в обоих Set
-let intersection = allPersonsSet.intersection(personsInOfficeTodaySet)
-// Set вне пересечения
-let subtracting = personsInOfficeTodaySet.subtracting(
-    personsInOfficeYesterdaySet)
-// Set уникальная разница
-let symmetricDifference = personsInOfficeTodaySet.symmetricDifference(
-    personsInOfficeYesterdaySet)
-// Объединение
-let union = personsInOfficeTodaySet.union(personsInOfficeYesterdaySet)
-
-print(intersection)
-print(subtracting)
-print(union)
-print(symmetricDifference)
-
-// Синтаксис
-let a = Set<String>(["Ivan", "Alex", "Oleg", "Anton", "Max"])
-let a1 = Set(["Ivan", "Alex", "Oleg", "Anton", "Max"])
-let a2: Set<String> = ["Ivan", "Alex", "Oleg", "Anton", "Max"]
-let b = Set(["Ivan", "Ivan", "Ivan", "Ivan", "Max"])  // Удалит при создании дубли
-print(b)
-
-// Изменение
-var c = Set(["Ivan", "Alex", "Oleg", "Anton", "Max"])
-c = ["Ivan", "Alex", "Oleg"]
-print(c)
-c.insert("Olga")
-print(c)
-c.remove("Ivan")
-print(c)
-
-// Проверка наличия
-print(c.contains("Olga"))
-
-// Перебор
-for element in c {
-    print(element)
-}
-
+print(multilineString)
