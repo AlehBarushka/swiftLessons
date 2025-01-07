49e87282d55f0a143aa5e3b3d719287ad694eb26
alehbarushka <1.alehbarushka@gmail.com>
2025-01-07 16:42:45 +0300

    lesson 31

--- swiftLessons/main.swift
+++ swiftLessons/main.swift
@@ -1,25 +1,80 @@
-import Darwin
 import Foundation
 
-let a = 45
-let b = 34.56454564
+// Поиск максимального значения в массиве
+let a = makeArray()
+var max = Int.min
 
-let string = "a: " + String(a) + " b: " + String(b)
+for element in a {
+    if element > max {
+        max = element
+    }
+}
 
-print(string)
+// Возведение каждого элемента массива в квадрат
+var b = makeArray()
 
-let concatString = "a: \(a) b: \(b) summ: \(a + 78)"
+for element in 0..<b.count {
+    b[element] *= b[element]
+}
 
-print(concatString)
+// Удаление чётных элементов массива
+var c = makeArray()
 
-let doubleQuotationMarks = "Привет \"Мир\""  // Двойные кавычки
-let multilineString = """
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic typesetting,
-    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    """
+for index in stride(from: c.count - 1, to: 0, by: -1) {
+    if c[index] % 2 == 0 {
+        c.remove(at: index)
+    }
+}
 
-print(multilineString)
+// Фильтрация массива с созданием нового c нечётными числами
+let d = makeArray()
+var newD = [Int]()
+
+for element in d {
+    if element % 2 != 0 {
+        newD.append(element)
+    }
+}
+
+// Сортировка массива по возрастанию (алгоритм сортировка выбором)
+var e = makeArray()
+
+for index in 0..<e.count {
+    for j in (index + 1)..<e.count {
+        if e[j] < e[index] {
+            let tmp = e[index]
+            e[index] = e[j]
+            e[j] = tmp
+        }
+    }
+}
+
+// Обмен значениями между двумя массивами (чётные в одном, нечётные в другом)
+var array1 = makeArray()
+var array2 = makeArray()
+
+for index in stride(from: array1.count - 1, through: 0, by: -1) {
+    if array1[index] % 2 == 0 {
+        array2.append(array1[index])
+        array1.remove(at: index)
+    }
+}
+
+for index in stride(from: array2.count - 1, through: 0, by: -1) {
+    if array2[index] % 2 != 0 {
+        array1.append(array2[index])
+        array2.remove(at: index)
+    }
+}
+
+print("asd")
+
+func makeArray() -> [Int] {
+    var a: [Int] = []
+
+    for _ in 0..<10 {
+        a.append(Int.random(in: 0...100))
+    }
+
+    return a
+}
